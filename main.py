85a7a09856c60fa80d2bd26478b73f80e27c4206
yanhenning <yanhenning@gmail.com>
2021-06-09 01:31:39 -0300

    add some changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
 import os
 from flask import Flask
-from google.cloud import datastore
 
 from api.datastore_gateway.datastore_gateway import DatastoreGateway
 
@@ -31,10 +31,19 @@ def create_user():
     return 'Usuario criado'
 
 
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+
+
 @app.put('/user/<string:username>')
 def edit_user(username):
     return f'Usuario {username} alterado'
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=8080, debug=True)
