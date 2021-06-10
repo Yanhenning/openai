713dd3b92fbc7b2e3728cf273348f38930c96f35
yanhenning <yanhenning@gmail.com>
2021-06-10 02:47:06 -0300

    feature - cria função para obter usuário por username e adiciona a validação de cpf e email únicos

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from flask import Flask, jsonify
 from flask import request
 
 from api.core.exceptions import ValidationError
-from api.core.user.create_user import insert_user
+from api.core.user.get_user_by_username import get_user_by_username
+from api.core.user.insert_user import insert_user
 from api.datastore_gateway.datastore_gateway import DatastoreGateway
 
 app = Flask(__name__)
@@ -17,17 +18,12 @@ def hello_world():
     return 'Hello, World!3'
 
 
-@app.route('/teste')
-def hello_world2():
-    client_usuario = DatastoreGateway('usuario')
-    entities = client_usuario.create(**{'username': 'Yan'})
-    return 'criado'
-
-
 @app.get('/user/<string:username>')
 def get_user(username):
-    client_usuario = DatastoreGateway('usuario')
-    return client_usuario.get(username=('=', username))
+    user = get_user_by_username(username)
+    if not user:
+        return {'username': f'User with {username} username does not exist'}, 404
+    return user, 200
 
 
 @app.post('/user')
@@ -36,16 +32,16 @@ def create_user():
         created_user = insert_user(request.json)
     except ValidationError as context:
         return jsonify(context.errors), 400
-    return created_user
+    return created_user, 200
 
 
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
+    return f"""
+    An internal error occurred: <pre>{e}</pre>
     See logs for full stacktrace.
-    """.format(e), 500
+    """, 500
 
 
 @app.put('/user/<string:username>')
